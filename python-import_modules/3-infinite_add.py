7a573ae48de98ec7f78cacd9a647d4716e83f619
abbassimedayoub <abbassimedayoub@gamil.com>
2024-01-24 22:15:27 +0100

    update

--- python-import_modules/3-infinite_add.py
+++ python-import_modules/3-infinite_add.py
@@ -1,17 +1,14 @@
 #!/usr/bin/python3
-def add_arg(argv):
-    n = len(argv) - 1
-    if n == 0:
-        print("{:d}".format(n))
-        return
-    else:
-        i = 1
-        add = 0
-        while i <= n:
-            add += int(argv[i])
-            i += 1
-        print("{:d}".format(add))
-
 if __name__ == "__main__":
-    import sys
-    add_arg(sys.argv)
+    from sys import argv
+
+    result = 0
+    args = len(argv)
+    if args == 0:
+        print("0")
+    elif args == 1:
+        print("{}".format(int(argv[1])))
+    else:
+        for i in range(1, args):
+            result += int(argv[i])
+        print("{:d}".format(result))
--- python-import_modules/5-variable_load.py
+++ python-import_modules/5-variable_load.py
@@ -1,10 +1,7 @@
 #!/usr/bin/python3
-from variable_load_5 import a
-
-
-def find_var():
-    print(a)
-
-
 if __name__ == "__main__":
-    find_var()
+    import hidden_4
+    list_names = dir(hidden_4)
+    for name in list_names:
+        if not name[:2] == '__':
+            print("{:s}".format(name))
