6b59752f262c5fea3b616975023c2a2c67252a03
abbassimedayoub <abbassimedayoub@gamil.com>
2024-01-29 22:29:02 +0100

    corcet the function name

--- python-data_structures/4-new_in_list.py
+++ python-data_structures/4-new_in_list.py
@@ -1,5 +1,5 @@
-#!/usr/bin/python3i
-def replace_in_list(my_list, idx, element):
+#!/usr/bin/python3
+def new_in_list(my_list, idx, element):
     new_list = my_list.copy()
     if idx in range(0, len(new_list)):
         new_list[idx] = element
