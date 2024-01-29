80c2ca0e98b17265bb5ee07959e7e8ac4199acc1
abbassimedayoub <abbassimedayoub@gamil.com>
2024-01-29 23:07:49 +0100

    empty line if noting entered

--- python-data_structures/6-print_matrix_integer.py
+++ python-data_structures/6-print_matrix_integer.py
@@ -1,9 +1,12 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
-    for i in range(0, len(matrix)):
-        for j in range(0, len(matrix[i])):
+    if not matrix or not matrix[0]:
+        print()
+        return
+
+    for i in range(len(matrix)):
+        for j in range(len(matrix[i])):
             if j != len(matrix[i]) - 1:
                 print("{:d}".format(matrix[i][j]), end=" ")
             else:
                 print("{:d}".format(matrix[i][j]))
-    print()
