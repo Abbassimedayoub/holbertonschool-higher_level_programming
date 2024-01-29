2fa7810e4bb41109596549c3a647b19ac105eadf
abbassimedayoub <abbassimedayoub@gamil.com>
2024-01-29 18:06:16 +0100

    auotpep8

--- python-data_structures/10-divisible_by_2.py
+++ python-data_structures/10-divisible_by_2.py
@@ -2,8 +2,8 @@
 def divisible_by_2(my_list=[]):
     if my_list:
         new_list = []
-        for i in range(0,len(my_list)):
+        for i in range(0, len(my_list)):
             new_list.append(my_list[i] % 2 == 0)
         return new_list
     else:
-        return None 
+        return None
