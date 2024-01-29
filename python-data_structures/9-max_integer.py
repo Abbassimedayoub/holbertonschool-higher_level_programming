fb47d8ed540683e63f0c6d04a3e8b386102b8fa3
abbassimedayoub <abbassimedayoub@gamil.com>
2024-01-29 22:36:46 +0100

    update code

--- python-data_structures/9-max_integer.py
+++ python-data_structures/9-max_integer.py
@@ -1,9 +1,13 @@
 #!/usr/bin/python3
-def max_integer(my_list=[]):
-    if len(my_list) == 0:
+def max_integer(my_list=None):
+    if my_list is None:
         return None
-     res = my_list[0]
-    for i in range(1, len(my_list)):
-        if my_list[i] > res:
-             res = my_list[i]
-    return rest
+
+    if len(my_list):
+        res = my_list[0]
+        for i in range(1, len(my_list)):
+            if my_list[i] > res:
+                res = my_list[i]
+        return res
+    else:
+        return None
