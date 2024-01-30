314991f48b5a2532b859ab494d2cce6595a22f56
abbassimedayoub <abbassimedayoub@gamil.com>
2024-01-30 15:12:51 +0100

    add all project files

---
+++ python-more_data_structures/2-uniq_add.py
@@ -0,0 +1,4 @@
+#!/user/bin/python3
+def uniq_add(my_list=[]):
+    new_list = list(set(my_list))
+    return sum(new_list)
---
+++ python-more_data_structures/5-number_keys.py
@@ -0,0 +1,3 @@
+#!/user/bin/python3
+def number_keys(a_dictionary):
+    return len(a_dictionary.keys())
