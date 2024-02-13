f8e9d05141e5fbbf87b926c0b17c7380a930d37c
abbassimedayoub <abbassimedayoub@gamil.com>
2024-02-13 15:46:22 +0100

    task4

---
+++ python-inheritance/4-inherits_from.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+""" the function cheks if the obejct is a subclass"""
+
+
+def inherits_from(obj, a_class):
+    """chack if the object is an instance"""
+    return issubclass(obj, a_class) and type(obj) != a_class
