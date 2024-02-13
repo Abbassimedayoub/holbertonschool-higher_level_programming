d65f695d38eb875de644470bd2601232f5f3860b
abbassimedayoub <abbassimedayoub@gamil.com>
2024-02-13 16:11:02 +0100

    create condition

---
+++ python-inheritance/7-base_geometry.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""cretae an empty class claed BaseGeometry"""
+
+
+class BaseGeometry:
+    """define a calss caled BaseGeometry"""
+
+    def area(self):
+        """define area method"""
+        raise Exception("area() is not implemented")
+
+    def integer_validator(self, name, value):
+        if type(value) != int:
+            """the value must be  an int """
+            raise TypeError("{} must be an integer".format(name))
+        if value < 0:
+            """the valu must be poitive int"""
+            raise ValueError("{} must be greater than 0".format(name))
