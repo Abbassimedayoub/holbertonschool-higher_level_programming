3942ced97fb1ca0437bbb1f0c59d6e70b10a5768
abbassimedayoub <abbassimedayoub@gamil.com>
2024-02-17 12:06:49 +0100

    synatxe error

--- python-input_output/5-save_to_json_file.py
+++ python-input_output/5-save_to_json_file.py
@@ -4,5 +4,5 @@ import json
 
 
 def save_to_json_file(my_obj, filename):
-    with open(filename, "w", encoding="utf-8") as file:
-        json.dump(my_obj, file)
+    with open(filename, "w", encoding="utf-8") as files:
+        json.dump(my_obj, files)
