641e4d1453dc8b1bba606b8e220a2e8b8e09e621
abbassimedayoub <abbassimedayoub@gamil.com>
2024-02-17 11:34:56 +0100

    change the permission from write r to append a

--- python-input_output/2-append_write.py
+++ python-input_output/2-append_write.py
@@ -10,5 +10,5 @@ def append_write(filename="", text=""):
     @filename: the file to be treated
     @text: the text to be added at the end of the file 
     """
-    with open(filename, 'w', encoding='utf-8') as file:
+    with open(filename, 'a', encoding='utf-8') as file:
         return file.write(text)
