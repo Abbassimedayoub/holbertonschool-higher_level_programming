6147cfe0cc648d9f29ad7c4b7674aa7db4aaf4fd
abbassimedayoub <abbassimedayoub@gamil.com>
2024-02-17 11:24:27 +0100

    function that append a text at the end of a file

---
+++ python-input_output/2-append_write.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""
+    function that add text at the end of a file
+    and return the number of added charcters
+"""
+
+
+def append_write(filename="", text=""):
+    """
+    @filename: the file to be treated
+    @text: the text to be added at the end of the file 
+    """
+    with open(filename, 'w', encoding='utf-8') as file:
+        file.write(text)
+    return len(text)
