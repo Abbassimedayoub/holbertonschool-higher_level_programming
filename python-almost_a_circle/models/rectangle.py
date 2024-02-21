72c730bd7ada1e9fa87f3d360ecd16fb8626defa
abbassimedayoub <abbassimedayoub@gamil.com>
2024-02-21 20:32:51 +0100

    setattr

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -87,7 +87,7 @@ class Rectangle(Base):
         return ("[Rectangle] ({}) {}/{} - {}/{}".format(
                 self.id, self.x, self.y, self.width, self.height))
 
-    def update(self, *args):
+    def update(self, *args, **kwargs):
         """Update the attributes of the Rectangle instance"""
         if args:
             if len(args) >= 1:
@@ -100,3 +100,7 @@ class Rectangle(Base):
                 self.x = args[3]
             if len(args) >= 5:
                 self.y = args[4]
+        else:
+            for key, value in kwargs.items():
+                setattr(self, key, value)
+    
