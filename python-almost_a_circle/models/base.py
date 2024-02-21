ae638d61e32d5ab9818968a1e8b5c3d36b2af735
abbassimedayoub <abbassimedayoub@gamil.com>
2024-02-21 21:35:50 +0100

    obj to JSON

--- python-almost_a_circle/models/base.py
+++ python-almost_a_circle/models/base.py
@@ -18,7 +18,7 @@ class Base:
     @staticmethod
     def to_json_string(list_dictionaries):
         """ dictionary to JSON format"""
-        if list_dictionaries is None or len(list_dictionaries):
+        if list_dictionaries is None or len(list_dictionaries) == 0:
             return "[]"
         else:
             return json.dumps(list_dictionaries)
