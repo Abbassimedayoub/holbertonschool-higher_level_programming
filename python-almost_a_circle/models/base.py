74dd661c8716fc3fa638aeea7b7c7fcdaa9f3752
abbassimedayoub <abbassimedayoub@gamil.com>
2024-02-21 21:38:56 +0100

    dumos

--- python-almost_a_circle/models/base.py
+++ python-almost_a_circle/models/base.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 """Base Module"""
+from json import dumps
 
 
 class Base:
@@ -18,7 +19,7 @@ class Base:
     @staticmethod
     def to_json_string(list_dictionaries):
         """ dictionary to JSON format"""
-        if list_dictionaries is None or len(list_dictionaries) == 0:
+        if list_dictionaries is None or not list_dictionaries == 0:
             return "[]"
         else:
             return json.dumps(list_dictionaries)
