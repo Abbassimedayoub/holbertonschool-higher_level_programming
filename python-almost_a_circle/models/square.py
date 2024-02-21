74916174012210292365a9663e7ade2c1e0f6559
abbassimedayoub <abbassimedayoub@gamil.com>
2024-02-21 20:53:36 +0100

    update

--- python-almost_a_circle/models/square.py
+++ python-almost_a_circle/models/square.py
@@ -1,17 +1,46 @@
-#!/usr/bin/python3
-""" Module containing class Square """
-from models.rectangle import Rectangle
-
-
-class Square(Rectangle):
-    """ Square class inherits from Rectangle class """
+class Square(Base):
+    """A class representing a square, inheriting from Base"""
 
     def __init__(self, size, x=0, y=0, id=None):
-        """ Initializes a Square instance """
-        super().__init__(size, size, x, y, id)
-        self.size = size
+        """
+        Initializes a Square instance.
+        """
+        super().__init__(id)  # Call the superclass constructor
+        self.size = size  # Set the size attribute
+        self.x = x  # Set the x-coordinate attribute
+        self.y = y  # Set the y-coordinate attribute
+
+    @property
+    def size(self):
+        """Getter method for the size attribute"""
+        return self.width  # Since width and height are the same for a square
+
+    @size.setter
+    def size(self, value):
+        """
+        Setter method for the size attribute.
+        """
+        if not isinstance(value, int) or value <= 0:
+            raise ValueError("Size must be a positive integer")
+        self.width = value  # Set both width and height to the same value
+        self.height = value
 
     def __str__(self):
-        """Return representation of string"""
-        return ("[Square] ({}) {}/{} - {}".format(self.id,
-                self.x, self.y, self.width))
+        """
+        Returns a string representation of the square.
+        """
+        return f"[Square] ({self.id}) {self.x}/{self.y} - {self.size}"
+
+    def update(self, *args):
+        """
+        Updates the attributes of the square using positional arguments.
+        """
+        if args:
+            if len(args) >= 1:
+                self.id = args[0]
+            if len(args) >= 2:
+                self.size = args[1]
+            if len(args) >= 3:
+                self.x = args[2]
+            if len(args) >= 4:
+                self.y = args[3]
