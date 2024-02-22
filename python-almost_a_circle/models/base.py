0b04dd8638a0af06c7f2972c49abe8b8efe5f71a
abbassimedayoub <abbassimedayoub@gamil.com>
2024-02-22 10:46:13 +0100

    autopep8

--- python-almost_a_circle/models/base.py
+++ python-almost_a_circle/models/base.py
@@ -24,11 +24,20 @@ class Base:
         else:
             return dumps(list_dictionaries)
 
+    @staticmethod
+    def to_json_string(list_dictionaries):
+        """JSON string representation of list_dictionaries"""
+        if list_dictionaries is None or len(list_dictionaries) == 0:
+            return "[]"
+        else:
+            return json.dumps(list_dictionaries)
+
     @classmethod
     def save_to_file(cls, list_objs):
-        """ that writes the JSON string representation"""
+        """Writes the JSON string representation of instances"""
         if list_objs is None:
             list_objs = []
+
         filename = cls.__name__ + ".json"
         json_string = cls.to_json_string([obj.to_dictionary()
                                          for obj in list_objs])
