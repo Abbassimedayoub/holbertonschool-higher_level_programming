7089b4b790b16d72380d838ed8e6acad4d01d58d
abbassimedayoub <abbassimedayoub@gamil.com>
2024-02-22 10:40:58 +0100

    base

--- python-almost_a_circle/models/base.py
+++ python-almost_a_circle/models/base.py
@@ -23,3 +23,49 @@ class Base:
             return "[]"
         else:
             return dumps(list_dictionaries)
+
+    @classmethod
+    def save_to_file(cls, list_objs):
+        """ that writes the JSON string representation"""
+        if list_objs is None:
+            list_objs = []
+        filename = cls.__name__ + ".json"
+        json_string = cls.to_json_string([obj.to_dictionary()
+                                         for obj in list_objs])
+        with open(filename, "w") as file:
+            file.write(json_string)
+      @staticmethod
+    def from_json_string(json_string):
+        """list of dictionaries represented by json_string"""
+        if json_string is None or len(json_string) == 0:
+            return []
+        else:
+            return json.loads(json_string)
+
+    @classmethod
+    def create(cls, **dictionary):
+        """instance with all attributes"""
+        if cls.__name__ == "Rectangle":
+            dummy = cls(1, 1)
+        elif cls.__name__ == "Square":
+            dummy = cls(1)
+        else:
+            dummy = cls()
+
+        dummy.update(**dictionary)
+        return dummy
+
+    @classmethod
+    def load_from_file(cls):
+        """Returns a list of instances"""
+        filename = cls.__name__ + ".json"
+        try:
+            with open(filename, "r") as file:
+                json_string = file.read()
+                dictionaries = cls.from_json_string(json_string)
+                instances = [cls.create(**dictionary)
+                             for dictionary in dictionaries]
+            return instances
+        except FileNotFoundError:
+            return []
+        
